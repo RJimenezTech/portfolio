fbec00ada2416cd08d47a7ba9801e8eb991c62c2
Robert Jimenez <robertjimenez86@gmail.com>
2022-08-07 00:23:23 -0500

    added about me info, needs stlying

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React from "react";
+import coverImage from "../../assets/images/cover-image.jpg";
 
 function About() {
-    return (
-        <h2> This is the About page</h2>
-    )
+  return (
+    <section className="about-me my-5">
+      <h1 id="about">About Me</h1>        
+      <div className="hero-image">
+          <img
+            src={coverImage}
+            className="my-2 hero-image"
+            style={{ width: "75%" }}
+            alt="headshot"/>
+        </div>
+      <div className="my-2">
+        <p>My name is Robert Jimenez. I'm a software and web developer.</p>
+        <p>
+          I'm an inquisitive and motivated engineer with tons of project
+          experience from a coding bootcamp on top of 10+ years of experience in
+          manufacturing. I'm always seeking to improve my backend programming
+          dexterity while maintaining frontend skills. I've taken up coding and
+          web development in recent years. Check out my portfolio at the link
+          above and my GitHub profile at the link below.
+        </p>
+        <p> 
+          Currently working as Safety Engineer, training and designing safety
+          features for machines in the gasket manufacturing industry with
+          extensive OSHA compliance experience. I also have substantial rubber
+          processing and rubber mold design experiwnce in the oil and gas
+          industry.
+        </p>
+        <p>
+          Language Learning <span>&#40;</span>human languages!
+          <span>&#41; </span>
+          has become a large part of my life as a great way to connect with
+          people and deconstruct complicated topics to make them more
+          manageable. You can often catch me on a language exchange call on
+          WhatsApp. Having learned Spanish to an intermediate level as an
+          adult, I’m currently dabbling in Modern Standard Arabic with some
+          Egyptian dialect in the mix. On the list for languages that I plan
+          on studying: French and American Sign Language. Let’s connect.  
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer () {
 
     return (
-        <footer>
+        <footer className="px-1 py-2">
             <h2>This is a footer</h2>
         </footer>
     )
