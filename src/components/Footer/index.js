39266e6b08ae78b6621d38ca7ecf0613538b9450
Robert Jimenez <robertjimenez86@gmail.com>
2022-08-08 21:01:26 -0500

    added icons and links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
 function Footer () {
+    const year = () => {
+        let currentTime = new Date();
+        return currentTime.getFullYear();
+    }
 
     return (
-        <footer className="px-1 py-2">
-            <h2>This is a footer</h2>
+        <footer>
+            <div className="row my-2 center-icons">
+                <div className="row col-12 my-2">
+                    <h2 className="col-3">
+                        <a href="mailTo:rjimeneztech@gmail.com">
+                            <i className="bi bi-envelope-fill"></i>
+                        </a>
+                    </h2>
+                    <h2 className="col-3">
+                        <a href="https://www.linkedin.com/in/rjimeneztech/" target="_blank" rel="noopener noreferrer">
+                            <i className="bi bi-linkedin"></i>
+                        </a>
+                    </h2>
+                    <h2 className="col-3">
+                        <a href="https://github.com/RJimenezTech" target="_blank" rel="noopener noreferrer">
+                            <i className="bi bi-github"></i>    
+                        </a>
+                        
+                    </h2>
+                    <h2 className="col-3">
+                        <a href="https://www.youtube.com/channel/UCiQPaVt0qpyvZSY_JWW4d0A" target="_blank" rel="noopener noreferrer">
+                            <i className="bi bi-youtube"></i>
+                        </a>
+                    </h2>
+                </div> 
+                <div className="col-12 my-1">&copy; Robert Jimenez {year()}</div> 
+            </div>
         </footer>
     )
 }
