4dfcbf56b086c6fe007a6b522ccada74663bc62b
Robert Jimenez <robertjimenez86@gmail.com>
2022-08-14 09:21:06 -0500

    updated responsivenss of project pics

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,27 +13,27 @@ function Portfolio() {
                 <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://tweezer-app.herokuapp.com/" target='_blank' rel='noreferrer noopener'>
                     <p className="p-1">Tweezer App - Twitter Clone</p>
-                    <img className="p-2" style={{height: "275px", width: "275px"}} src={tweezer} alt="twitter clone"></img>
+                    <img className="p-2 project-img" src={tweezer} alt="twitter clone"></img>
                 </a>
                 <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://habitualtracker.herokuapp.com/" target='_blank' rel='noreferrer noopener'>
                     <p className="p-1">Habitual - Habit Tracker</p>
-                    <img className="p-2" style={{height: "275px", width: "275px"}} src={habit} alt="habit tracker"></img>
+                    <img className="p-2 project-img" src={habit} alt="habit tracker"></img>
                 </a>
                 <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://rjimeneztech.github.io/MovieFinder/" target='_blank' rel='noreferrer noopener'>
                     <p className="p-1">Occasional Movie - Movie Finder</p>
-                    <img className="p-2" style={{height: "275px", width: "275px"}} src={occasional} alt="movie finder"></img>
+                    <img className="p-2 project-img" src={occasional} alt="movie finder"></img>
                 </a>
                 <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://rjimeneztech.github.io/weather-dashboard/" target='_blank' rel='noreferrer noopener'>
                     <p className="p-1">Weather Dashboard - Forecaster</p>
-                    <img className="p-2" style={{height: "275px", width: "275px"}} src={weather} alt="weather app"></img>
+                    <img className="p-2 project-img" src={weather} alt="weather app"></img>
                 </a>
                 <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://rjimeneztech.github.io/workday-scheduler/" target='_blank' rel='noreferrer noopener'>
                     <p className="p-1">Workday Schedule - Daily Agenda</p>
-                    <img className="p-2" style={{height: "275px", width: "275px"}} src={schedule} alt="daily agenda"></img>
+                    <img className="p-2 project-img" src={schedule} alt="daily agenda"></img>
                 </a>   
             </div>
 
