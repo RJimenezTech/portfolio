eb2e0c391657194b7613f1d35a898753b300947e
Robert Jimenez <robertjimenez86@gmail.com>
2022-08-14 17:44:47 -0500

    added blue/orange color scheme throughout

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,28 +6,28 @@ function Contact() {
             
             <div className="w-75 w-lg-50 mt-5">
                 <h4>Find me all over the web:</h4>
-                <button type="button" className="btn btn-light w-100 border border-dark m-2">
-                    <a className="text-decoration-none text-dark contact-button" href="mailto:rjimeneztech@gmail.com" target="_blank" rel='noreferrer noopener'>
+                <button type="button" className="btn w-100 border border-dark m-2">
+                    <a className="text-decoration-none contact-button" href="mailto:rjimeneztech@gmail.com" target="_blank" rel='noreferrer noopener'>
                     <i className="bi bi-envelope-fill"></i> rjimeneztech@gmail.com  
                     </a>
                 </button>
-                <button type="button" className="btn btn-light w-100 border border-dark m-2">
-                    <a className="text-decoration-none text-dark contact-button" href="https://www.linkedin.com/in/rjimeneztech/" target="_blank" rel='noreferrer noopener'>
+                <button type="button" className="btn w-100 border border-dark m-2">
+                    <a className="text-decoration-none contact-button" href="https://www.linkedin.com/in/rjimeneztech/" target="_blank" rel='noreferrer noopener'>
                     <i className="bi bi-linkedin"></i> LinkedIn: Robert Jimenez
                     </a>
                 </button>
-                <button type="button" className="btn btn-light w-100 border border-dark m-2">
-                    <a className="text-decoration-none text-dark contact-button" href="https://github.com/RJimenezTech" target="_blank" rel='noreferrer noopener'>
+                <button type="button" className="btn w-100 border border-dark m-2">
+                    <a className="text-decoration-none contact-button" href="https://github.com/RJimenezTech" target="_blank" rel='noreferrer noopener'>
                     <i className="bi bi-github"></i> Github: rjimeneztech
                     </a>
                 </button>
-                <button type="button" className="btn btn-light w-100 border border-dark m-2">
-                    <a className="text-decoration-none text-dark contact-button" href="https://twitter.com/rjimeneztech" target="_blank" rel='noreferrer noopener'>
+                <button type="button" className="btn w-100 border border-dark m-2">
+                    <a className="text-decoration-none contact-button" href="https://twitter.com/rjimeneztech" target="_blank" rel='noreferrer noopener'>
                     <i className="bi bi-twitter"></i> @RJimenezTech
                     </a>
                 </button>
-                <button type="button" className="btn btn-light w-100 border border-dark m-2">
-                    <a className="text-decoration-none text-dark contact-button" href="https://www.youtube.com/channel/UCiQPaVt0qpyvZSY_JWW4d0A" target="_blank" rel='noreferrer noopener'>
+                <button type="button" className="btn w-100 border border-dark m-2">
+                    <a className="text-decoration-none contact-button" href="https://www.youtube.com/channel/UCiQPaVt0qpyvZSY_JWW4d0A" target="_blank" rel='noreferrer noopener'>
                     <i className="bi bi-youtube"></i> YouTube: RJimenezTech
                     </a>
                 </button>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,30 +10,30 @@ function Portfolio() {
         <section className="container mb-5">
             <h3 className="my-4">Check out some of my projects below!</h3> 
             <div className="row d-flex justify-content-center justify-content-md-start align-items-center flex-wrap mb-5">
-                <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
+                <a className="project-card col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://tweezer-app.herokuapp.com/" target='_blank' rel='noreferrer noopener'>
-                    <p className="p-1">Tweezer App - Twitter Clone</p>
-                    <img className="p-2 project-img" src={tweezer} alt="twitter clone"></img>
+                    <p className="project-header p-1 mb-0">Tweezer App - Twitter Clone</p>
+                    <img className="p-2 project-img mb-2" src={tweezer} alt="twitter clone"></img>
                 </a>
-                <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
+                <a className="project-card col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://habitualtracker.herokuapp.com/" target='_blank' rel='noreferrer noopener'>
-                    <p className="p-1">Habitual - Habit Tracker</p>
-                    <img className="p-2 project-img" src={habit} alt="habit tracker"></img>
+                    <p className="project-header p-1 mb-0">Habitual - Habit Tracker</p>
+                    <img className="p-2 project-img mb-2" src={habit} alt="habit tracker"></img>
                 </a>
-                <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
+                <a className="project-card col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://rjimeneztech.github.io/MovieFinder/" target='_blank' rel='noreferrer noopener'>
-                    <p className="p-1">Occasional Movie - Movie Finder</p>
-                    <img className="p-2 project-img" src={occasional} alt="movie finder"></img>
+                    <p className="project-header p-1 mb-0">Occasional Movie - Movie Finder</p>
+                    <img className="p-2 project-img mb-2" src={occasional} alt="movie finder"></img>
                 </a>
-                <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
+                <a className="project-card col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://rjimeneztech.github.io/weather-dashboard/" target='_blank' rel='noreferrer noopener'>
-                    <p className="p-1">Weather Dashboard - Forecaster</p>
-                    <img className="p-2 project-img" src={weather} alt="weather app"></img>
+                    <p className="project-header p-1 mb-0">Weather Dashboard - Forecaster</p>
+                    <img className="p-2 project-img mb-2" src={weather} alt="weather app"></img>
                 </a>
-                <a className="project-card border border-light col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
+                <a className="project-card col-8 col-md-6 col-lg-4 text-decoration-none text-body" 
                 href="https://rjimeneztech.github.io/workday-scheduler/" target='_blank' rel='noreferrer noopener'>
-                    <p className="p-1">Workday Schedule - Daily Agenda</p>
-                    <img className="p-2 project-img" src={schedule} alt="daily agenda"></img>
+                    <p className="project-header p-1 mb-0">Workday Schedule - Daily Agenda</p>
+                    <img className="p-2 project-img mb-2" src={schedule} alt="daily agenda"></img>
                 </a>   
             </div>
 
